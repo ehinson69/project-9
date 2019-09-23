869a13cec65fea9c631100fe24bc6982e66c2794
Elizabeth Hinson <elizabeth.hinson@verizon.com>
2019-09-23 16:25:52 -0500

    Base project works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// setup a friendly greeting for the root route
+router.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the REST API project!"
+  });
 });
 
 module.exports = router;
